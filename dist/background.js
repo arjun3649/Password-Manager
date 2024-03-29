ed5b37d1b557e92a652c51f3562d450c4240b925
arjun3649 <arjun.rakwal3649@gmail.com>
2024-03-29 21:26:36 +0530

    boilerplate

--- dist/background.js
+++ dist/background.js
@@ -4,9 +4,6 @@ var __webpack_exports__ = {};
   !*** ./src/background/background.js ***!
   \**************************************/
 // TODO: background script
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: on installed function
-});
 
 /******/ })()
 ;
--- src/contentScript/contentScript.js
+++ src/contentScript/contentScript.js
@@ -1,1 +1,40 @@
 // TODO: content script
+// content_script.js
+console.log("====================================");
+console.log("content script injected");
+console.log("====================================");
+
+// function extractCredentials() {
+//   const emailField = document.getElementsByTagName("#email");
+//   const passwordField = document.querySelector("#password");
+
+//   if (emailField && passwordField) {
+//     const loginId = emailField.value;
+//     const password = passwordField.value;
+//     console.log("Email:", loginId);
+//     console.log("Password:", password);
+//   }
+// }
+
+// // Add event listener to the form submission
+// const loginForm = document.querySelector("#login-form");
+// loginForm.addEventListener("submit", extractCredentials);
+
+let Field = document.getElementsByTagName("input");
+console.log(Field);
+
+const length = Field.length;
+
+for (let i = 0; i < length; i++) {
+  const Input = Field.item[i];
+  console.log(Input);
+  // if (Input.type == "email") {
+  //   const email = Field[i].value;
+  //   console.log(true);
+  // }
+  // if (InputType !== "password") continue;
+  // if (InputType == "email") {
+  //   const email = Field[i].value;
+  //   console.log(email);
+  // }
+}
